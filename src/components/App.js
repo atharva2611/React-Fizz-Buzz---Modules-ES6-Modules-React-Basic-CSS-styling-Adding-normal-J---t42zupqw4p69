6fdf3fc67182b94bb70ea0a446179cddfeaea7ad
16bitash <entropy.ash@gmail.com>
2023-01-17 21:10:01 +0530

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
+  const [count, setCount] = useState(1);
 
-  
+  const getCountClassName = () => {
+    let className = "";
+
+    if (count % 3 === 0) {
+      className += "fizz";
+    }
+
+    if (count % 5 === 0) {
+      className += "buzz";
+    }
+
+    if (className === "") {
+      className = "normal";
+    }
+
+    return className;
+  };
+
   return (
-    <div id="main">
-
-    </div>
-  )
-}
-
+    <>
+      <button id="increment" onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+      <div id="counter" className={getCountClassName()}>
+        {count}
+      </div>
+      <button id="decrement" onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+    </>
+  );
+};
 
 export default App;
